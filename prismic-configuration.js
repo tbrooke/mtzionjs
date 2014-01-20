0d6987740a1eb35d30ad184bd6b576d11031d67e
tbrooke <tom.brooke@gmail.com>
2014-01-20 18:55:01 -0500

    set up prismic

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -1,13 +1,13 @@
 exports.Configuration = {
 
-  apiEndpoint: 'https://lesbonneschoses.prismic.io/api',
+  apiEndpoint: 'https://brookelaw.prismic.io/api',
 
   // -- Access token if the Master is not open
-  // accessToken: 'xxxxxx',
+  // accessToken: 'MC5VdHJBWGdFQUFENWZrQ0ZH.WO-_ve-_ve-_vX1jJe-_ve-_vTcgYA5377-977-9YO-_ve-_ve-_vUzvv73vv73vv73vv71t77-977-977-9Vu-_ve-_vQ',
 
   // OAuth
-  // clientId: 'xxxxxx',
-  // clientSecret: 'xxxxxx',
+  // clientId: 'UtrAXgEAACdfkCFF',
+  // clientSecret: 'e5e9335d42ba94973440c67b844f87c8',
 
   // -- Links resolution rules
   linkResolver: function(ctx, doc) {
