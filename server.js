0337d1e0ed0f70254baafb034d2a3225bcbf4150
tbrooke <tom.brooke@gmail.com>
2014-01-20 19:38:52 -0500

    setup websocket

---
+++ server.js
@@ -0,0 +1,87 @@
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+var WebSocketServer = require('ws').Server
+var http = require('http');
+
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    routes = require('./routes'),
+    http = require('http'),
+    path = require('path'),
+    engine = require('ejs-locals'),
+    prismic = require('./prismic-helpers');
+
+var app = express();
+
+// all environments
+app.engine('ejs', engine);
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('1234'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// Routes
+app.get('/', routes.index);
+app.get('/documents/:id/:slug', routes.detail);
+app.get('/search', routes.search);
+
+// OAuth - Routes
+app.get('/signin', prismic.signin);
+app.get('/auth_callback', prismic.authCallback);
+app.post('/signout', prismic.signout);
+
+/**
+var PORT = process.env.PORT || 3000;
+
+
+http.createServer(app).listen(PORT, function() {
+  console.log('Express server listening on port ' + PORT);
+});
+
+ */
+
+/**
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	  response.write("Welcome to Node.js on OpenShift!\n\n");
+	  response.end("Thanks for visiting us! \n");
+});
+*/
+
+var server = http.createServer(app);
+
+server.listen( port, ipaddress, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+wss = new WebSocketServer({
+    server: server,
+    autoAcceptConnections: false
+});
+wss.on('connection', function(ws) {
+  console.log("New connection");
+  ws.on('message', function(message) {
+    ws.send("Received: " + message);
+  });
+  ws.send('Welcome!');
+});
+
+console.log("Listening to " + ipaddress + ":" + port + "...");
